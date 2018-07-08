42b8083fdb4fe284fd95915db2c4fb28385fad07
Michael Bamford <michael@michaelbamford.com>
2018-07-08 18:25:50 +1000

    Create React web app with QR reader

---
+++ web/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Test from './Test'
+// import logo from './logo.svg';
+import logo from './Ceva-96_96.png'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header> */}
+        {/* <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        <div className='QrScanner'>
+          <Test />
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    );
+  }
+}
+
+export default App;
