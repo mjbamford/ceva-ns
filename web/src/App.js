d5391376a0df348f9cca6fec6c826f6fc8ba4507
Michael Bamford <michael@michaelbamford.com>
2018-07-10 17:02:21 +1000

    Add material-ui/core

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import SimpleAppBar from './SimpleAppBar'
 import Test from './Test'
 // import logo from './logo.svg';
 import logo from './Ceva-96_96.png'
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <SimpleAppBar />
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
