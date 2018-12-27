dd8e9a017c2ba4bfd23041cb37fc4547a8afc6c0
Michael Bamford <michael@michaelbamford.com>
2018-12-27 18:31:38 +1100

    Add Sku to Products

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -26,6 +26,7 @@ ActiveAdmin.register Product do
           image_tag product.image.variant resize: '100x100'
         end
       end
+      column :sku
       column :name
       column :description
       column :updated_at
@@ -35,6 +36,7 @@ ActiveAdmin.register Product do
   show do
     attributes_table do
       row :name
+      row :sku
       row :description
       row :image do |product|
         image_tag product.image.variant resize: '300x300'
@@ -49,7 +51,8 @@ ActiveAdmin.register Product do
     semantic_errors
     inputs do
       input :name
-      input :image, as: :file
+      input :image, as: :file, hint: image_tag(f.object.image.variant(resize: '300x300'))
+      input :sku
       input :description
       input :information
       input :indications
