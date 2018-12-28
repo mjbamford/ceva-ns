b1129b435e4e9d4dc6b3d593955553006ac16a1e
Michael Bamford <michael@michaelbamford.com>
2018-12-28 19:25:19 +1100

    Add datasheet preview for stocks

--- app/admin/stocks.rb
+++ app/admin/stocks.rb
@@ -4,10 +4,31 @@ ActiveAdmin.register Stock do
 
   permit_params :product_id, :region_id, :datasheet
 
+  show do
+    attributes_table do
+      row :product
+      row :region
+      if resource.datasheet.attached?
+        path = rails_blob_path resource.datasheet, disposition: 'attachment'
+        html = if resource.datasheet.previewable?
+          image_tag resource.datasheet.preview resize: '300x300'
+        else
+          'download'
+        end
+        row('datasheet') { link_to html, path }
+      end
+      row :code
+    end
+  end
+
   form do |f|
     inputs "Stock details" do
       input :region, as: :radio
-      input :datasheet, as: :file
+      options = { as: :file }
+      if f.object.datasheet.previewable?
+        options[:hint] = image_tag f.object.datasheet.preview resize: '300x300'
+      end
+      input :datasheet, options
     end
     actions
   end
