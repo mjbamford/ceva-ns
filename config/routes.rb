77ace0ff4ba40d27d72e54c0ed1838ba0c9470d5
Michael Bamford <michael@michaelbamford.com>
2018-12-31 04:41:57 +1100

    Revert "Add json format constraint for '/q' route"
    This reverts commit 05a266a4aa170bd0a4569735345db0a06ddd5671.

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,14 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  scope '/api' do
+  scope 'api' do
     resources :products, only: %i[ index show ]
     resources :datasheets, param: :code, only: :show
   end
 
-  get 'q/:code', to: 'datasheets#show',
-    as: 'codified_datasheet', constraints: { format: 'json'}
-
-  get '*path', to: "application#index", constraints: -> (req) do
-    !req.xhr? && req.format.html?
+  with_options to: 'application#index',
+      constraints: -> req { !req.xhr? && req.format.html? } do |react|
+    react.get 'q/:code', as: 'codified_datasheet'
+    react.get '*path'
   end
 end
